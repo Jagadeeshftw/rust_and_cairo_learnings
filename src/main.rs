433c4a2f851b261a04e8c3f4ceaeb611b9e26d27
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-10-11 19:35:51 +0530

    Initializing Deck project

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
-#[derive(Debug)]
+#[derive(Debug)]     // Its kind of importing the Debug traits.
+                     // Traits contains collection of functions.
 
 struct Deck {
     cards: Vec<String>
 }
 
 fn main() {
-    let deck = Deck { cards: vec![]};
-    println!("Heres your deck: {:?}", deck);
+
+    let suites = ["Hearts", "Spades", "Diamonds"];
+    let values = ["Ace", "Two", "Three"];
+
+    // Variables will be called as bindings in rust and all the
+    // bindings will be immutable by default.
+    // specifying `mut` will make the bindings as mutable(can able to modify)
+    let mut cards = vec![];    
+
+    for suite in suites{
+        for value in values {
+            let card = format!("{} of {}", value, suite);
+            cards.push(card);
+        }
+    }
+
+    let deck = Deck { cards};
+    println!("Heres your deck: {:#?}", deck); // here {:?} itself will work
+                                              // adding {:#?} will print the items
+                                              // in the nicely formatted lines
+                                              
+    
 }
